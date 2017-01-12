466fcb7c45c036b811249d3975272a5c344cf731
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-01-11 18:39:30 -0600

    Added automated installation for jq as well.

--- install_ucrypt.sh
+++ install_ucrypt.sh
@@ -1,12 +1,21 @@
 #! /bin/sh
 
+# not in typical RHEL repos..
+# sudo which yum 2>/dev/null && sudo yum install -q -y jq
+sudo which yum 2>/dev/null && ( \
+    curl -# https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 \
+    sudo tee - > /usr/bin/jq >/dev/null ; sudo chmod +x /usr/bin/jq)
+
+sudo which dnf 2>/dev/null && sudo dnf install -q -y jq
+sudo which apt-get 2>/dev/null && sudo apt-get install -qqy jq
+
 # Install both binaries
 
 curl -# https://raw.githubusercontent.com/userify/ucrypt/master/ucrypt.py \
-    | sudo tee - > /opt/userify-server/ucrypt.py
+    | sudo tee - > /opt/userify-server/ucrypt.py >/dev/null
 
 curl -# https://raw.githubusercontent.com/userify/ucrypt/master/disable_mfa.sh \
-    | sudo tee - > /opt/userify-server/disable_mfa.sh
+    | sudo tee - > /opt/userify-server/disable_mfa.sh >/dev/null
 
 chmod +x /opt/userify-server/disable_mfa.sh /opt/userify-server/ucrypt.py
 
