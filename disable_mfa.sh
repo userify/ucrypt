046a8dcec3dbc46d47a034603f4621d4c1be8413
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-01-11 18:53:42 -0600

    Check for file existence.

--- disable_mfa.sh
+++ disable_mfa.sh
@@ -10,8 +10,14 @@ fi
 
 echo "Please provide the username or email address to disable MFA for."
 read UNAME
+UNAMEFILE="$DATAPATH/$UNAME:username"
 
-user_id="$(sudo cat $DATAPATH/$UNAME:username:content_type | sudo $UCRYPT -i - -o - | jq -r .user_id)"
+if [ ! -f "$UNAMEFILE" ]; then
+    echo "$UNAMEFILE does not exist."
+    exit 1
+fi
+
+user_id="$(sudo cat $UNAMEFILE | sudo $UCRYPT -i - -o - | jq -r .user_id)"
 user_filename=$DATAPATH/$user_id:user
 redis_keyname=user:/opt/userify-server/data/:$user_id:user::$user_id:
 
