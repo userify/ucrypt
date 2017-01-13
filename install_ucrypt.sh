a61cb54deb3486ba1c8363edcaecea106a0e5d8c
Jamieson Becker <jamieson@jamiesonbecker.com>
2017-01-13 10:02:07 -0600

    Cleaned up RHEL prereqs

--- install_ucrypt.sh
+++ install_ucrypt.sh
@@ -3,13 +3,9 @@
 # RHEL/CENTOS PREREQUISITES
 function rhel_prereqs {
     echo "Installing RHEL/CENT/Amazon Prerequisites"
-    # not in typical RHEL repos..
-    # sudo which yum 2>/dev/null && sudo yum install -q -y jq
-    sudo which yum 2>/dev/null && ( \
     # Annoying behavior of RHEL: error status if 'nothing to do'
     set +e
-    sudo yum install -q -y jq python-devel libffi-devel openssl-devel libxml2-devel \
-        gcc gcc-c++
+    sudo yum install -q -y jq python-devel libffi-devel openssl-devel libxml2-devel gcc gcc-c++
     set -e
     curl -# "https://bootstrap.pypa.io/get-pip.py" | sudo /usr/bin/env python
 }
@@ -19,8 +15,7 @@ function debian_prereqs {
     echo "Installing Debian/Ubuntu Prerequisites"
     sudo apt-get update
     sudo apt-get -qy upgrade
-    sudo apt-get install -qqy build-essential python-dev libffi-dev zlib1g-dev \
-    libssl-dev 
+    sudo apt-get install -qqy build-essential python-dev libffi-dev zlib1g-dev libssl-dev 
     curl -# "https://bootstrap.pypa.io/get-pip.py" | sudo -H /usr/bin/env python
 }
 
@@ -28,7 +23,7 @@ function debian_prereqs {
 sudo which yum 2>/dev/null && rhel_prereqs
 sudo which apt-get 2>/dev/null && debian_prereqs
 
-sudo pip install pynacl ucrypt
+sudo $(which pip) install pynacl ucrypt
 
 # Install both binaries
 
